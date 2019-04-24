350a64404b54e5dd134cd8628180e882f0cafac6
Sameh Ossama Koleid <samehossama@hotmail.com>
2019-04-25 00:33:21 +0200

    MISC: Add python 3 shebang

--- project.py
+++ project.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
